c2c55332780ee2257ae095eea4285952a1d0f7bd
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2023-01-31 05:07:56 -0800

    Cleaning up the HTML, CSS & Javascript by adding comments and formatting the code.

--- flippages.js
+++ flippages.js
@@ -1,38 +1,39 @@
+//======Helper function to check where count is to update state and user functionality======
 const checkCount = (count, backBtn) => {
-    if (count > 0) {
-        backBtn.style.pointerEvents = 'auto';
-        backBtn.style.opacity = '1';
-    } else {
-        backBtn.style.pointerEvents = 'none';
-        backBtn.style.opacity = '0';
-    }
+  if (count > 0) {
+    backBtn.style.pointerEvents = "auto";
+    backBtn.style.opacity = "1";
+  } else {
+    backBtn.style.pointerEvents = "none";
+    backBtn.style.opacity = "0";
+  }
 };
-
+//======Exports on the reducer functions handling what state each section should be in relative to the view port======
 export const opacity = (sec) => {
-    sec.style.opacity = '1';
+  sec.style.opacity = "1";
 };
 
 export const startOver = (count, backBtn) => {
-    count = 0;
-    checkCount(count, backBtn);
-    const sections = document.querySelectorAll('.sec');
-    for (let k = sections.length - 1; k > -1; k--) {
-        sections[k].style.transform = `translateX(${k}00vw)`;
-    }
-}; 
+  count = 0;
+  checkCount(count, backBtn);
+  const sections = document.querySelectorAll(".sec");
+  for (let k = sections.length - 1; k > -1; k--) {
+    sections[k].style.transform = `translateX(${k}00vw)`;
+  }
+};
 
 export const bringInNext = (count, backBtn) => {
-    checkCount(count, backBtn);
-    const sections = document.querySelectorAll('.sec');
-    sections.forEach((sec) => {
-        sec.style.transform = `translateX(-${count * 100}vw)`;
-    });
+  checkCount(count, backBtn);
+  const sections = document.querySelectorAll(".sec");
+  sections.forEach((sec) => {
+    sec.style.transform = `translateX(-${count * 100}vw)`;
+  });
 };
 
 export const bringInPrev = (count, backBtn) => {
-    checkCount(count, backBtn)
-    const sections = document.querySelectorAll('.sec');
-    sections.forEach((sec) => {
-        sec.style.transform = `translateX(-${count}00vw)`;
-    });
+  checkCount(count, backBtn);
+  const sections = document.querySelectorAll(".sec");
+  sections.forEach((sec) => {
+    sec.style.transform = `translateX(-${count}00vw)`;
+  });
 };
--- script.js
+++ script.js
@@ -1,74 +1,74 @@
 import { bringInNext, bringInPrev, startOver, opacity } from "./flippages.js";
-
+//=====+Declaring state======
 const no = document.querySelector('[data-="no"]');
-const backBtn = document.querySelector('.back-btn');
-const NextBtns = document.querySelectorAll('.next');
-const numberToAdd = document.querySelector('.number-to-add');
-const answer = document.querySelector('.answer');
-const restart = document.querySelector('.restart');
-const end = document.querySelector('.end');
-const noYouDont = document.querySelector('.no-you-dont');
-const sections = document.querySelectorAll('.sec');
+const backBtn = document.querySelector(".back-btn");
+const NextBtns = document.querySelectorAll(".next");
+const numberToAdd = document.querySelector(".number-to-add");
+const answer = document.querySelector(".answer");
+const restart = document.querySelector(".restart");
+const end = document.querySelector(".end");
+const noYouDont = document.querySelector(".no-you-dont");
+const sections = document.querySelectorAll(".sec");
 const vw = window.innerWidth;
 let count = 0;
-
+//======Handling a fade in effect when moving sections======
 const initaiteOpacity = () => {
-    sections[0].style.opacity = '1';
-    sections.forEach((sec) => {
-        sec.addEventListener('transitionend', () => {
-            const right = sec.getBoundingClientRect().right;
-            if (right >= vw -5 && right <= vw + 5) opacity(sec);
-            else sec.style.opacity = '0';
-        });
+  sections[0].style.opacity = "1";
+  sections.forEach((sec) => {
+    sec.addEventListener("transitionend", () => {
+      const right = sec.getBoundingClientRect().right;
+      if (right >= vw - 5 && right <= vw + 5) opacity(sec);
+      else sec.style.opacity = "0";
     });
+  });
 };
 initaiteOpacity();
-
+//======Calculating and storing the secret variable======
 const calc = () => {
-    numberToAdd.innerHTML = Math.round(Math.random() * 50);
-    answer.innerHTML = numberToAdd.innerHTML / 2;
+  numberToAdd.innerHTML = Math.round(Math.random() * 50);
+  answer.innerHTML = numberToAdd.innerHTML / 2;
 };
 calc();
-
+//=====Alert modal function======
 const toggleAlert = () => {
-    const alert = document.querySelector('.alert');
-    const backdrop = document.querySelector('.backdrop');
-    alert.classList.toggle('show');
-    backdrop.classList.toggle('show');
+  const alert = document.querySelector(".alert");
+  const backdrop = document.querySelector(".backdrop");
+  alert.classList.toggle("show");
+  backdrop.classList.toggle("show");
 };
-
-noYouDont.addEventListener('click', () => {
+//======Adiing event listeners======
+noYouDont.addEventListener("click", () => {
+  toggleAlert();
+  setTimeout(() => {
     toggleAlert();
-    setTimeout(() => {
-        toggleAlert();
-    }, 2000);
+  }, 2000);
 });
-end.addEventListener('click', () => {
-    document.location.reload();
-    window.close();
+end.addEventListener("click", () => {
+  document.location.reload();
+  window.close();
 });
-restart.addEventListener('click', () => {
-    startOver(count, backBtn);
-    count = 0;
-    setTimeout(() => {
-        calc();
-    }, 1000);
+restart.addEventListener("click", () => {
+  startOver(count, backBtn);
+  count = 0;
+  setTimeout(() => {
+    calc();
+  }, 1000);
 });
-no.addEventListener('click', () => {
+no.addEventListener("click", () => {
+  toggleAlert();
+  setTimeout(() => {
     toggleAlert();
-    setTimeout(() => {
-        toggleAlert();
-    }, 2000);
+  }, 2000);
 });
-backBtn.addEventListener('click', () => {
-    count--;
-    bringInPrev(count, backBtn);
-    initaiteOpacity();
+backBtn.addEventListener("click", () => {
+  count--;
+  bringInPrev(count, backBtn);
+  initaiteOpacity();
 });
 NextBtns.forEach((btn) => {
-    btn.addEventListener('click', () => {
-        count++;
-        bringInNext(count, backBtn);
-        initaiteOpacity();
-    });
+  btn.addEventListener("click", () => {
+    count++;
+    bringInNext(count, backBtn);
+    initaiteOpacity();
+  });
 });
